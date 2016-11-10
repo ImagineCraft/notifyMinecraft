a5c41ec61719fe704aa8b37223c1156f024f48b9
Armen Kaleshian <armen@kriation.com>
2016-11-10 16:31:27 -0500

    Add logic to send notification of registration through RCON

--- event/listener.php
+++ event/listener.php
@@ -3,6 +3,8 @@
 namespace ImagineCraft\notifyMinecraft\event;
 
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use phpbb\config\config;
+use ImagineCraft\notifyMinecraft\event\rcon;
 
 class listener implements EventSubscriberInterface
 {
@@ -16,7 +18,7 @@ class listener implements EventSubscriberInterface
 	/* @var \phpbb\config\config */
 	protected $config;
 
-	public function __construct(\phpbb\config\config $config)
+	public function __construct($config)
 	{
 		$this->config = $config;
 	}
@@ -25,9 +27,28 @@ class listener implements EventSubscriberInterface
 	{
 		if( $this->config['require_activation'] == USER_ACTIVATION_ADMIN )
 		{
+			// Retrieve necessary data for message
+			$username = $event['user_row']['username'];
+			$regtime = gmdate("H:i:s T",
+				$event['user_row']['user_regdate']);
+			$boardurl = generate_board_url();
+			$message = "phpBB: User $username registered at $regtime "
+				. "requires activation here: $boardurl";
 
+			$mc_server = $this->config['ic_nm_server_host'];
+			$mc_port = $this->config['ic_nm_server_port'];
+			$mc_pass = $this->config['ic_nm_server_pass'];
+			$mc_timeout = $this->config['ic_nm_server_timeout'];
+			// TODO
+			// $mc_message = $this->config['ic_nm_server_message'];
+
+			$rcon = new rcon($mc_server, $mc_port, $mc_pass, $mc_timeout);
+			if( $rcon->connect() )
+			{
+				$rcon->send_command("say $message");
+				$rcon->disconnect();
+			}
 		}
-
 
 	}
 }
