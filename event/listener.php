cf98259787724ae70c58ba9b11b5eccf7e679a7d
Armen Kaleshian <armen@kriation.com>
2016-11-16 11:11:03 -0500

    Remove dependency on activation by administrator

--- event/listener.php
+++ event/listener.php
@@ -25,31 +25,27 @@ class listener implements EventSubscriberInterface
 
 	public function notify_minecraft($event)
 	{
-		if( $this->config['require_activation'] == USER_ACTIVATION_ADMIN )
+		// Retrieve necessary data for message
+		$username = $event['user_row']['username'];
+		$regtime = gmdate("H:i:s T",
+			$event['user_row']['user_regdate']);
+		$boardurl = generate_board_url();
+		$message = "phpBB: User $username registered at $regtime "
+			. "requires activation here: $boardurl";
+
+		$mc_server = $this->config['ic_nm_server_host'];
+		$mc_port = $this->config['ic_nm_server_port'];
+		$mc_pass = $this->config['ic_nm_server_pass'];
+		$mc_timeout = $this->config['ic_nm_server_timeout'];
+		// TODO
+		// $mc_message = $this->config['ic_nm_server_message'];
+
+		$rcon = new rcon($mc_server, $mc_port, $mc_pass, $mc_timeout);
+		if( $rcon->connect() )
 		{
-			// Retrieve necessary data for message
-			$username = $event['user_row']['username'];
-			$regtime = gmdate("H:i:s T",
-				$event['user_row']['user_regdate']);
-			$boardurl = generate_board_url();
-			$message = "phpBB: User $username registered at $regtime "
-				. "requires activation here: $boardurl";
-
-			$mc_server = $this->config['ic_nm_server_host'];
-			$mc_port = $this->config['ic_nm_server_port'];
-			$mc_pass = $this->config['ic_nm_server_pass'];
-			$mc_timeout = $this->config['ic_nm_server_timeout'];
-			// TODO
-			// $mc_message = $this->config['ic_nm_server_message'];
-
-			$rcon = new rcon($mc_server, $mc_port, $mc_pass, $mc_timeout);
-			if( $rcon->connect() )
-			{
-				$rcon->send_command("say $message");
-				$rcon->disconnect();
-			}
+			$rcon->send_command("say $message");
+			$rcon->disconnect();
 		}
-
 	}
 }
 
