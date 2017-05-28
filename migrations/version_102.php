21277d4d12854afaf85df888e904cc0d5509b1c7
Armen Kaleshian <armen@kriation.com>
2017-05-27 22:54:41 -0400

    Add forum numeric id for application trigger.

---
+++ migrations/version_102.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace ImagineCraft\notifyMinecraft\migrations;
+
+use phpbb\db\migration\migration;
+
+class version_102 extends migration 
+{
+	public function effectively_installed()
+	{
+		// TODO!
+		return true;
+	}
+	
+	static public function depends_on()
+	{   
+		return array('\ImagineCraft\notifyMinecraft\migrations\version_100');
+	}
+
+	public function update_data()
+	{   
+		return array(
+			array('config.add', array('ic_nm_forum_id', 'forum id')),
+		};
+
+	}
+}
+
